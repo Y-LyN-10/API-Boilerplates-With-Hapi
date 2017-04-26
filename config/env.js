a86c1078a5d1eb37ac3b56130072bd53a6771d54
Yulia Tenincheva <yulia.tenincheva@mentormate.com>
2017-04-26 15:50:21 +0300

    Updated google oauth authentication configuration

--- config/env.js
+++ config/env.js
@@ -1,10 +1,9 @@
 const envKey = key => {
   const env = process.env.NODE_ENV || 'development';
-
+  
   const configuration = {
     host: process.env.HOST,
     port: process.env.PORT || 80,
-    securePort: process.env.SSL_PORT || 443,
     jwt_secret: process.env.JWT_SECRET,
     google_client_id: process.env.GOOGLE_CLIENT_ID,
     google_client_secret: process.env.GOOGLE_CLIENT_SECRET
