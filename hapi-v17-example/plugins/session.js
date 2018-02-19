7deff63ee4edbc3140f5a5c693423d1764ea7b93
Yulia Tenincheva <yulia.tenincheva@mentormate.com>
2018-02-19 20:17:48 +0200

    Updated session plugin

--- hapi-v17-example/plugins/session.js
+++ hapi-v17-example/plugins/session.js
@@ -1,45 +1,22 @@
-exports.register = function (server, pluginOptions) {  
-  const cache = server.cache({
-    segment: 'sessions',
-    expiresIn: 3 * 24 * 60 * 60 * 1000
-  });
-  
-  server.method('authenticate', async function(token, account) {
-    account.scope = (account.rank === 3) ? 'admin' : 'user';
+exports.register = function (server, pluginOptions) {
+  // Official example from https://github.com/hapijs/hapi-auth-cookie
+  // TODO: re-write this
+  const cache = server.cache({ segment: 'sessions', expiresIn: 3 * 24 * 60 * 60 * 1000 });
+  server.app.cache = cache;
 
-    try {
-      await cache.set(token, { account: account }, 0);      
-      return false;
-    } catch (err) {
-      request.server.log(['error', 'auth'], err);
-      return err;
-    }
-  });  
-
-  server.state('token', {
-    ttl: null,
-    isSecure: true,
-    isHttpOnly: true,
-    encoding: 'base64json',
-    clearInvalid: true, // remove invalid cookies
-    strictHeader: true  // don't allow violations of RFC 6265
-  });
-
-  //Setup the social Google+ login strategy
   server.auth.strategy('session', 'cookie', {
-    password: pluginOptions.cookieSecret || process.env.COOKIE_SECRET, // cookie secret, min 32 chars long
-    cookie: 'connect.sid', // default cookie name
-    clearInvalid: true,
-    redirectTo: '/', // if 'next' param is given
-    redirectOnTry: false,
-    isSecure: false, // required for non-https applications
-    ttl: 24 * 60 * 60 * 1000,
-    validateFunc: async function (request, session) {
+    password: 'password-should-be-32-characters',
+    cookie: 'sid-example',
+    redirectTo: '/login',
+    isSecure: false,
+    validateFunc: async (request, session) => {
       const cached = await cache.get(session.sid);
-      const out = { valid: !!cached };
+      const out = {
+          valid: !!cached
+      };
 
       if (out.valid) {
-        out.credentials = cached.account;
+          out.credentials = cached.account;
       }
 
       return out;
